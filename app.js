a6b404405099922c1203bcfdc8d0b27b2ac24e3d
esther-v <esvalas@gmail.com>
2021-10-13 14:22:13 +0200

    check for row/column of 3

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     const width = 8
     const squares = []
+    let score = 0
 
     const candyColors = [
         'red',
@@ -27,8 +28,11 @@ document.addEventListener('DOMContentLoaded', () => {
     createBoard()
 
     //Drag the candies
+    let colorBeingDragged
+    let colorBeingReplaced
+    let squareIdBeingDragged
+    let squareIdBeingReplaced
 
-    
     squares.forEach(square => square.addEventListener('dragstart', dragStart))
     squares.forEach(square => square.addEventListener('dragend', dragEnd))
     squares.forEach(square => square.addEventListener('dragover', dragOver))
@@ -37,18 +41,19 @@ document.addEventListener('DOMContentLoaded', () => {
     squares.forEach(square => square.addEventListener('drop', dragDrop))
 
     function dragStart() {
+        colorBeingDragged = this.style.backgroundColor
+        squareIdBeingDragged = parseInt(this.id)
+        console.log(colorBeingDragged)
         console.log(this.id, 'dragstart')
     }
 
-    function dragEnd() {
-        console.log(this.id, 'dragend')
-    }
-
-    function dragOver() {
+    function dragOver(e) {
+        e.preventDefault()
         console.log(this.id, 'dragover')
     }
 
-    function dragEnter() {
+    function dragEnter(e) {
+        e.preventDefault()
         console.log(this.id, 'dragenter')
     }
 
@@ -58,8 +63,70 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function dragDrop() {
         console.log(this.id, 'dragdrop')
+        colorBeingReplaced = this.style.backgroundColor
+        squareIdBeingReplaced = parseInt(this.id)
+        this.style.backgroundColor = colorBeingDragged
+        squares[squareIdBeingDragged].style.backgroundColor = colorBeingReplaced
     }
 
+    function dragEnd() {
+        console.log(this.id, 'dragend')
+        //what is a valid move?
+        let validMoves = [
+            squareIdBeingDragged -1, 
+            squareIdBeingDragged -width,
+            squareIdBeingDragged +1,
+            squareIdBeingDragged +width
+        ]
+        let validMove = validMoves.includes(squareIdBeingReplaced)
 
+        if (squareIdBeingReplaced && validMove) {
+            squareIdBeingReplaced = null
+        } else if (squareIdBeingReplaced && !validMove) {
+            squares[squareIdBeingReplaced].style.backgroundColor = colorBeingReplaced
+            squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged
+        } else squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged
+    }
+
+    //----Checking for matches
+    // check for row of three
+    function checkRowForThree() {
+        for( i = 0; i < 61; i++) {
+            let rowOfThree = [i, i+1, i+2]
+            let decidedColor = squares[i].style.backgroundColor
+            const isBlank = squares[i].style.backgroundColor === ''
+
+            if(rowOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+                score += 3
+                rowOfThree.forEach(index => {
+                    squares[index].style.backgroundColor = ''
+                })
+            }
+        }
+    }
+    checkRowForThree()
+
+    // check for column of three
+    function checkColumnForThree() {
+        for( i = 0; i < 47; i++) {
+            let columnOfThree = [i, i+width, i+width*2]
+            let decidedColor = squares[i].style.backgroundColor
+            const isBlank = squares[i].style.backgroundColor === ''
+
+            if(columnOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+                score += 3
+                columnOfThree.forEach(index => {
+                    squares[index].style.backgroundColor = ''
+                })
+            }
+        }
+    }
+    checkColumnForThree()
+
+    window.setInterval(function() {
+        checkRowForThree()
+        checkColumnForThree()
+    }, 100)
+    
 
 })
