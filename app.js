cc4f44cbf551e861531fe08576d420d57ba5ff51
esther-v <esvalas@gmail.com>
2021-10-13 12:57:33 +0200

    js drag and drop

--- app.js
+++ app.js
@@ -3,13 +3,63 @@ document.addEventListener('DOMContentLoaded', () => {
     const width = 8
     const squares = []
 
+    const candyColors = [
+        'red',
+        'yellow',
+        'orange',
+        'purple',
+        'green',
+        'blue'
+    ]
+
     //create board
     function createBoard() {
         for (let i = 0; i < width*width; i++) {
             const square = document.createElement('div')
+            square.setAttribute('draggable', true)
+            square.setAttribute('id', i)
+            let randomColor = Math.floor(Math.random() * candyColors.length)
+            square.style.backgroundColor = candyColors[randomColor]
             grid.appendChild(square)
             squares.push(square)
         }
     }
     createBoard()
+
+    //Drag the candies
+
+    
+    squares.forEach(square => square.addEventListener('dragstart', dragStart))
+    squares.forEach(square => square.addEventListener('dragend', dragEnd))
+    squares.forEach(square => square.addEventListener('dragover', dragOver))
+    squares.forEach(square => square.addEventListener('dragenter', dragEnter))
+    squares.forEach(square => square.addEventListener('dragleave', dragLeave))
+    squares.forEach(square => square.addEventListener('drop', dragDrop))
+
+    function dragStart() {
+        console.log(this.id, 'dragstart')
+    }
+
+    function dragEnd() {
+        console.log(this.id, 'dragend')
+    }
+
+    function dragOver() {
+        console.log(this.id, 'dragover')
+    }
+
+    function dragEnter() {
+        console.log(this.id, 'dragenter')
+    }
+
+    function dragLeave() {
+        console.log(this.id, 'dragleave')
+    }
+
+    function dragDrop() {
+        console.log(this.id, 'dragdrop')
+    }
+
+
+
 })
